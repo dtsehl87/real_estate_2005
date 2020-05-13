282c4f3db72a9d508952b524b5e6ae3b5322c502
Dan Sehl <dan.sehl@gmail.com>
2020-05-13 09:51:47 -0600

    Write passing iteration 3

--- lib/house.rb
+++ lib/house.rb
@@ -6,10 +6,40 @@ class House
     @price = price
     @address = address
     @rooms = []
+    @details = {}
   end
 
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    if @price.gsub('$', '').to_i >= 500_000
+      true
+    else
+      false
+    end
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    result = 0
+    @rooms.each do |room|
+      result += room.area
+    end
+    result
+  end
+
+  def details
+    @details["price"] = @price.gsub('$', '').to_i
+    @details["address"] = @address
+    @details
+  end
+
 
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -33,4 +33,61 @@ class HouseTest < Minitest::Test
     assert_equal [room_1, room_2], house.rooms
   end
 
+  def test_it_is_avbove_market_average_if_500k_and_avove
+    house_1 = House.new("$400000", "123 sugar lane")
+
+    assert_equal false, house_1.above_market_average?
+
+    house_2 = House.new("$500000", "321 apple drive")
+
+    assert_equal true, house_2.above_market_average?
+
+    house_3 = House.new("$500001", "456 nowhere street")
+
+    assert_equal true, house_3.above_market_average?
+  end
+
+  def test_rooms_from_a_category_can_be_called
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal [room_1, room_2], house.rooms_from_category(:bedroom)
+    assert_equal [room_4], house.rooms_from_category(:basement)
+  end
+
+  def test_house_area_updates_after_rooms_added
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal 1900, house.area
+  end
+
+  def test_house_can_have_hash_of_details
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+    room_3 = Room.new(:living_room, 25, '15')
+    room_4 = Room.new(:basement, 30, '41')
+    house.add_room(room_1)
+    house.add_room(room_2)
+    house.add_room(room_3)
+    house.add_room(room_4)
+
+    assert_equal ({"price" => 400000, "address" => "123 sugar lane"}), house.details
+  end
+
 end
